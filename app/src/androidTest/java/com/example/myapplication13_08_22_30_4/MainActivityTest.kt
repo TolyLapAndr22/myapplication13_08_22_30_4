4d3399a6dc863a0fb561c3a1905bfba98ccb3749
aalapandr22 <aalap73@mail.ru>
2022-08-13 20:51:05 +0300

    myapplication13_08_22_30_4 MainActivityTest

---
+++ app/src/androidTest/java/com/example/myapplication13_08_22_30_4/MainActivityTest.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication13_08_22_30_4
+
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.any
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityScenario = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test() {
+    }
+}
